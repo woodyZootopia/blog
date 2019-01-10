a1815624cf1be3aca7aab7d0f909165a9640ca06
woody <piequalsabout314159@gmail.com>
2019-01-09 20:04:17 -0500

    new articles backup Wed  9 Jan 2019 20:04:17 EST

--- pngtojpg.sh
+++ pngtojpg.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# move all images to rawimages directory
 find content -name '*.png' | while read f
 do
     relfileplace=${f%.*}
     filename=${relfileplace##*/}
     absfileplace=`pwd`/${f%.*}
-    if [ ! -f "${absfileplace}.jpg" ]
-    then
-        sips -s format jpeg "${absfileplace}.png" -s formatOptions normal --out "${absfileplace}.jpg"
-    fi
-
     absdirplace=${absfileplace%/*}
     reldirplace=${absdirplace#*blog/}
-    newdirplace=./rawfiles/$reldirplace
+    newdirplace=./rawimages/$reldirplace
 
     mkdir -p "$newdirplace"
     # echo $newdirplace$filename
     mv "${absfileplace}.png" "$newdirplace${filename}.png"
 done
 
+# convert to jpg and write them to each directory
+find rawimages -name '*.png' | while read f
+do
+    filewithoutext=${f%.*}
+    relfileplace=${filewithoutext#*/}
+
+    sips -s format jpeg "./rawimages/${relfileplace}.png" -s formatOptions normal --out "${relfileplace}.jpg"
+    # echo ./rawimages/"$relfileplace".png
+    # echo ${relfileplace}.jpg
+done
